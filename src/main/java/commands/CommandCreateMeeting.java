d0b04b429db6776515eaf34cf8cbd1c62395cf46
Alexsey <shavrov.a.k@gmail.com>
2019-09-15 15:33:33 +0300

    add user command

--- src/main/java/commands/CommandCreateMeeting.java
+++ src/main/java/commands/CommandCreateMeeting.java
@@ -4,7 +4,9 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import dao.MeetingDAO;
+import dao.UserDAO;
 import entities.Meeting;
+import entities.User;
 
 public class CommandCreateMeeting implements Command {
 
@@ -14,12 +16,16 @@ public class CommandCreateMeeting implements Command {
 		MessageCommandOut messageOut = new MessageCommandOut(message);
 		String[] args = message.getMessage().split(" ");
 		MeetingDAO meetingDAO = new MeetingDAO();
+		UserDAO userDAO = new UserDAO();
 		try {
-			Meeting meeting = new Meeting(new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(args[2]+" "+args[3]), args[1]);
+			Meeting meeting = new Meeting(new SimpleDateFormat("yyyy-MM-dd HH:mm").parse(args[2] + " " + args[3]),
+					args[1]);
 			meetingDAO.insert(meeting);
 			messageOut.setText("Встреча создана.");
-			messageOut.addButton("Добавить пользователя.", "/addUser");
-			listMessagesOut.add(messageOut);
+			for (User user : userDAO.getAll()) {
+				messageOut.addButton("Добавить пользователя: " + user.getName(), "/addUser");
+				listMessagesOut.add(messageOut);
+			}
 			return listMessagesOut;
 		} catch (Exception e) {
 			e.printStackTrace();
